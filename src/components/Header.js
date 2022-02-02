e9feebfb4eae58aeb4d05e2aeb38068a63f8e6a0
Niibix <t0nani00@students.oamk.fi>
2022-02-02 14:43:54 +0200

    Lasse-setä

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return <div className="headerOuter">
+
+      <div className="headerInner">
+      <div className="otsikko"> Puuha-peten ostoslista</div>
+        <div className="navbtn">Peten koksut</div>
+        <div className="navbtn">Luottotiedot</div>
+        <div className="navbtn">Viina kassa</div>
+        <div className="navbtn">Heroiini</div>
+      
+      </div>
+  </div>;
+}
